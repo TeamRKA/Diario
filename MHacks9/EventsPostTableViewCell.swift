cbe33160be84a9898d035764c31ba99b131932f1
ryujimano <ryujimano@gmail.com>
2017-04-15 23:34:33 -0400

    add post table view cell

---
+++ MHacks9/EventsPostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventsPostTableViewCell.swift
+//  MHacks9
+//
+//  Created by Ryuji Mano on 4/15/17.
+//  Copyright © 2017 DeeptanhuRyujiKenanAvi. All rights reserved.
+//
+
+import UIKit
+
+class EventsPostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
